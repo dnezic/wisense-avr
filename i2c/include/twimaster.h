9cff33b2695211cf90a73b5e4f9fffab0a5b01c4
dnezic <drazen.nezic@svesoftware.com>
2014-05-03 00:01:07 +0200

    I2C slave added.

--- i2c/include/twimaster.h
+++ i2c/include/twimaster.h
@@ -87,6 +87,19 @@
 #define USI_OVERFLOW_VECTOR USI_OVF_vect
 #endif
 
+#if defined( __AVR_ATtiny85__ )
+#define DDR_USI             DDRB
+#define PORT_USI            PORTB
+#define PIN_USI             PINB
+#define PORT_USI_SDA        PORTB0
+#define PORT_USI_SCL        PORTB2
+#define PIN_USI_SDA         PINB0
+#define PIN_USI_SCL         PINB2
+#define USI_START_COND_INT  USISIF
+#define USI_START_VECTOR    USI_START_vect
+#define USI_OVERFLOW_VECTOR USI_OVF_vect
+#endif
+
 // General defines
 #define TRUE  1
 #define FALSE 0
